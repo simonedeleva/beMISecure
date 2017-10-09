751c5ef7ae43035670cb2ab1acba46d82d0b1eeb
Simone <simone.deleva05@gmail.com>
2017-10-09 15:28:39 +0000

    bug corretto su indice stazioni

--- db/migrate/20170401143529_remove_position_from_stations.rb
+++
@@ -1,5 +0,0 @@
-class RemovePositionFromStations < ActiveRecord::Migration[5.0]
-  def change
-    remove_column :stations, :Positions, :string
-  end
-end
--- db/migrate/20170401143718_add_lat_long_address_to_stations.rb
+++
@@ -1,7 +0,0 @@
-class AddLatLongAddressToStations < ActiveRecord::Migration[5.0]
-  def change
-    add_column :stations, :lat, :decimal
-    add_column :stations, :long, :decimal
-    add_column :stations, :address, :string
-  end
-end
